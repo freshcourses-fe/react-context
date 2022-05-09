2e167a28201bd98f66086c0137e8a52a4e64bb5b
Kirill <freshjs.ftd.@gmail.com>
2022-05-09 19:24:22 +0300

    Refactor Sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,30 +1,21 @@
 import React from 'react';
 import cx from 'classnames';
-import { UserContext, ThemeContext } from '../../contexts';
 import CONSTANTS from '../../constants';
 import styles from './Sidebar.module.scss';
+import { withTheme, withUser } from './../../HOCs';
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+  const { user, theme } = props;
+  const sidebarClassNames = cx(styles.container, {
+    [styles.darkTheme]: theme === CONSTANTS.THEMES.DARK,
+    [styles.lightTheme]: theme === CONSTANTS.THEMES.LIGTH,
+    [styles.nightTheme]: theme === CONSTANTS.THEMES.NIGHT,
+  });
   return (
-    <ThemeContext.Consumer>
-      {([theme]) => {
-        const sidebarClassNames = cx(styles.container, {
-          [styles.darkTheme]: theme === CONSTANTS.THEMES.DARK,
-          [styles.lightTheme]: theme === CONSTANTS.THEMES.LIGTH,
-          [styles.nightTheme]: theme === CONSTANTS.THEMES.NIGHT,
-        });
-        return (
-          <UserContext.Consumer>
-            {(user) => (
-              <article className={sidebarClassNames}>
-                <h1>{`${user.firstName} ${user.lastName}`}</h1>
-              </article>
-            )}
-          </UserContext.Consumer>
-        );
-      }}
-    </ThemeContext.Consumer>
+    <article className={sidebarClassNames}>
+      <h1>{`${user.firstName} ${user.lastName}`}</h1>
+    </article>
   );
 };
 
-export default Sidebar;
+export default withUser(withTheme(Sidebar));
